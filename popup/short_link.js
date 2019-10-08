50141e41ee52814dbbabc91676e1c8272414c635
Jason Howell <jasonrobot64@gmail.com>
2019-10-07 19:55:48 -0700

    I guess I refactored this to use a class at some point

--- popup/short_link.js
+++ popup/short_link.js
@@ -4,11 +4,12 @@
 
 /* eslint-disable no-use-before-define */
 export {
-    copyShortLinkToClipboard,
-    getSpecialProductId,
-    shortenAmazonLink,
+    // copyShortLinkToClipboard,
+    // getSpecialProductId,
+    // shortenAmazonLink,
+    ShortLink,
     UnshortenableUrlException,
-    $updateShortLink,
+    // $updateShortLink,
 };
 /* eslint-enable no-use-before-define */
 
@@ -17,83 +18,104 @@ export {
  */
 class UnshortenableUrlException extends Error {}
 
-function isAmazonComUrl( link ) {
-    return link.hostname.match( /amazon\.co/i ) === null;
-}
+class ShortLink {
+    /**
+     * @param {Url} [linkUrl=] Url to shorten. Defaults to the current tab's URL.
+     */
+    constructor( linkUrl ) {
+        if ( linkUrl !== undefined ) {
+            this.fullLink = linkUrl;
+        } else {
+            this.fullLink = this.getCurrentTabUrl();
+        }
 
-/**
- * @param {URL} [link]
- * @return {(String|null)}
- */
-function getSpecialProductId( link ) {
-    const productIdPart = link.pathname.match( /\/(?:dp|gp\/product)\/([\w\d]+)/ );
-
-    if ( productIdPart ) {
-        return productIdPart[1];
-    }
-    return null;
-}
-
-/**
- * Shorten an amazon.com product link.
- * @param {URL} [link] The full link to an amazon.com product.
- * @returns {URL} The short URL to the same product.
- */
-function shortenAmazonLink( link ) {
-    if ( ( link instanceof URL ) === false ) {
-        throw new TypeError( 'Argument must be instanceof URL.' );
+        this.setShortLink = ShortLink.shortenAmazonLink( this.fullLink );
     }
 
-    if ( isAmazonComUrl( link ) ) {
-        throw new UnshortenableUrlException( 'Can only shorten Amazon links.' );
+    get getShortLink() {
+        return this.shortLink;
     }
 
-    const shortenedLink = new URL( link.toString() );
-    const specialId = getSpecialProductId( link );
-
-    if ( specialId === null ) {
-        throw new UnshortenableUrlException( 'Not a product.' );
+    set setShortLink( link ) {
+        this.shortLink = link;
     }
 
-    shortenedLink.pathname = specialId;
-    shortenedLink.search = '';
-    shortenedLink.hostname = 'amzn.com';
-    return shortenedLink;
-}
-
-/**
- * Get the URL of the current tab.
- * @returns {URL}
- * @throws {Error} When the current tab's url isnt a valid url (wtf?)
- */
-async function getCurrentTabUrl() {
-    const [ { url: urlString } ] = await browser.tabs.query( {
-        currentWindow: true,
-        active: true,
-    } );
-    return new URL( urlString );
-}
-
-/**
- * Update the link to the shortened URL.
- */
-async function $updateShortLink() {
-    const shortLinkAnchor = document.querySelector( '.short-link__link' );
-
-    const longUrl = await getCurrentTabUrl();
-    try {
-        const shortLinkString = shortenAmazonLink( longUrl ).toString();
-
-        shortLinkAnchor.innerText = shortLinkString;
-        shortLinkAnchor.setAttribute( 'href', shortLinkString );
-    } catch ( exception ) {
-        shortLinkAnchor.innerText = exception.message;
+    static isAmazonComUrl( link ) {
+        return link.hostname.match( /amazon\.co/i ) === null;
     }
-}
 
-async function copyShortLinkToClipboard() {
-    const longUrl = await getCurrentTabUrl();
+    /**
+     * @param {URL} [link]
+     * @return {(String|null)}
+     */
+    static getSpecialProductId( link ) {
+        const productIdPart = link.pathname.match( /\/(?:dp|gp\/product)\/([\w\d]+)/ );
 
-    const shortLinkString = shortenAmazonLink( longUrl ).toString();
-    navigator.clipboard.writeText( shortLinkString );
+        if ( productIdPart ) {
+            return productIdPart[1];
+        }
+        return null;
+    }
+
+    /**
+     * Shorten an amazon.com product link.
+     * @param {URL} [link] The full link to an amazon.com product.
+     */
+    static shortenAmazonLink( link ) {
+        if ( ( link instanceof URL ) === false ) {
+            throw new TypeError( 'Argument must be instanceof URL.' );
+        }
+
+        if ( ShortLink.isAmazonComUrl( link ) ) {
+            throw new UnshortenableUrlException( 'Can only shorten Amazon links.' );
+        }
+
+        const shortenedLink = new URL( link.toString() );
+        const specialId = ShortLink.getSpecialProductId( link );
+
+        if ( specialId === null ) {
+            throw new UnshortenableUrlException( 'Not a product.' );
+        }
+
+        shortenedLink.pathname = specialId;
+        shortenedLink.search = '';
+        shortenedLink.hostname = 'amzn.com';
+
+        return shortenedLink;
+    }
+
+    /**
+     * Get the URL of the current tab.
+     * @returns {URL}
+     * @throws {Error} When the current tab's url isnt a valid url (wtf?)
+     */
+    static async getCurrentTabUrl() {
+        const [ { url: urlString } ] = await browser.tabs.query( {
+            currentWindow: true,
+            active: true,
+        } );
+        return new URL( urlString );
+    }
+
+    /**
+     * Update the link to the shortened URL.
+     */
+    async $updateShortLink() {
+        const shortLinkAnchor = document.querySelector( '.short-link__link' );
+
+        const longUrl = await ShortLink.getCurrentTabUrl();
+        try {
+            const shortLinkString = this.shortenAmazonLink( longUrl ).toString();
+
+            shortLinkAnchor.innerText = shortLinkString;
+            shortLinkAnchor.setAttribute( 'href', shortLinkString );
+        } catch ( exception ) {
+            shortLinkAnchor.innerText = exception.message;
+        }
+    }
+
+    async copyShortLinkToClipboard() {
+        const shortLinkString = this.getShortLink.toString();
+        navigator.clipboard.writeText( shortLinkString );
+    }
 }
--- popup/short_link.test.js
+++ popup/short_link.test.js
@@ -1,7 +1,5 @@
 import {
-    copyShortLinkToClipboard,
-    getSpecialProductId,
-    shortenAmazonLink,
+    ShortLink,
     UnshortenableUrlException,
 } from './short_link.js';
 
@@ -11,7 +9,7 @@ describe( 'short_link.js', () => {
 
         const expectedShortenedLink = new URL( 'https://amzn.com/B07QRLNW9B' );
 
-        const actualShortenedLink = shortenAmazonLink( testLink );
+        const actualShortenedLink = new ShortLink( testLink ).getShortLink;
 
         expect( actualShortenedLink.toString() )
             .toEqual( expectedShortenedLink.toString() );
@@ -22,46 +20,48 @@ describe( 'short_link.js', () => {
 
         const expectedShortenedLink = new URL( 'https://amzn.com/123456' );
 
-        const actualShortenedLink = shortenAmazonLink( testLink );
+        const actualShortenedLink = new ShortLink( testLink ).getShortLink;
 
         expect( actualShortenedLink.pathname )
             .toEqual( expectedShortenedLink.pathname );
     } );
 
+    /* eslint-disable no-unused-expressions */
     it( 'should not modify the original link', () => {
         const originalString = 'https://www.amazon.com/Chon/dp/B07QRLNW9B?pf_rd_p=5cc0ab18-ad5f-41cb-89ad-d43149f4e286&pd_rd_wg=0YiVS&pf_rd_r=BBQGR1ZWZBMTFG2TZG4D&ref_=pd_gw_wish&pd_rd_w=yEzK2&pd_rd_r=56e3b574-f148-4f3e-9ab8-555ecf3de5cf';
 
         const testLink = new URL( 'https://www.amazon.com/Chon/dp/B07QRLNW9B?pf_rd_p=5cc0ab18-ad5f-41cb-89ad-d43149f4e286&pd_rd_wg=0YiVS&pf_rd_r=BBQGR1ZWZBMTFG2TZG4D&ref_=pd_gw_wish&pd_rd_w=yEzK2&pd_rd_r=56e3b574-f148-4f3e-9ab8-555ecf3de5cf' );
 
-        shortenAmazonLink( testLink );
+        new ShortLink( testLink ).getShortLink;
 
         expect( testLink.toString() ).toEqual( originalString );
     } );
+    /* eslint-enable no-unused-expressions */
 
     it( 'should throw an exception when given an invalid argument', () => {
-        expect( () => shortenAmazonLink( 'www.amazon.com' ) ).toThrow( TypeError );
+        expect( () => new ShortLink( 'www.amazon.com' ) ).toThrow( TypeError );
     } );
 
     it( 'should throw an exception if the URL given is not for amazon.com', () => {
-        expect( () => shortenAmazonLink( new URL( 'http://google.com' ) ) )
+        expect( () => new ShortLink( new URL( 'http://google.com' ) ) )
             .toThrow( UnshortenableUrlException );
     } );
 
     it( 'should throw an exception on non product amazon pages', () => {
-        expect( () => shortenAmazonLink( new URL( 'http://amazon.com' ) ) )
+        expect( () => new ShortLink( new URL( 'http://amazon.com' ) ) )
             .toThrow( UnshortenableUrlException );
     } );
 
     describe( 'getSpecialProductId', () => {
         it( 'should work on a \'B\' link', () => {
-            const id = getSpecialProductId(
+            const id = ShortLink.getSpecialProductId(
                 new URL( 'http://a.co/dp/B123456?foo=bar' ),
             );
             expect( id ).toEqual( 'B123456' );
         } );
 
         it( 'should work on a book link', () => {
-            const id = getSpecialProductId(
+            const id = ShortLink.getSpecialProductId(
                 new URL( 'http://a.co/dp/123456/foo/bar' ),
             );
             expect( id ).toEqual( '123456' );
@@ -70,13 +70,13 @@ describe( 'short_link.js', () => {
         it( 'should work on a gp/product link', () => {
             const url = new URL( 'https://www.amazon.com/gp/product/B07CJN69MV/ref=s9_acsd_al_bw_c_x_3_w?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-5&pf_rd_r=F3CNC9EYPP3PA4X4S4XF&pf_rd_t=101&pf_rd_p=559a880c-b191-4be9-9883-341b6eabf178&pf_rd_i=19461882011' );
 
-            const id = getSpecialProductId( url );
+            const id = ShortLink.getSpecialProductId( url );
 
             expect( id ).toEqual( 'B07CJN69MV' );
         } );
 
         it( 'should return null when there is no ID', () => {
-            const id = getSpecialProductId(
+            const id = ShortLink.getSpecialProductId(
                 new URL( 'http://a.co/whatever/B123456?foo=bar' ),
             );
             expect( id ).toBeNull();
@@ -107,14 +107,11 @@ describe( 'short_link.js', () => {
             expect( navigator.clipboard.writeText() ).toBeInstanceOf( Promise );
         } );
 
-        fit( 'should copy a shortened link to the clipboard', async ( done ) => {
-            global.browser = {
-                tabs: {
-                    query: jest.fn( () => new Promise( ( resolve ) => resolve( [ { url: 'http://amazon.com/dp/B123456?foo=bar' } ] ) ) ),
-                },
-            };
+        it( 'should copy a shortened link to the clipboard', async ( done ) => {
+            const testLink = new URL( 'http://amazon.com/dp/B123456?foo=bar' );
 
-            await copyShortLinkToClipboard();
+            const shortLink = new ShortLink( testLink );
+            await shortLink.copyShortLinkToClipboard();
             expect( navigator.clipboard.writeText ).toHaveBeenCalled();
             done();
         } );
